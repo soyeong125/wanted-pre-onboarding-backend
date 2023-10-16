1abee1708b02519aac84d03b58cc09dd24eafd8e
choisoyeong <thdud3410@gmail.com>
2023-10-17 00:40:33 +0900

    Rename: 채용 지역 필드 추가 및 생성자 롬복 추가

--- src/main/java/com/example/wanted/dto/recruit/request/RecruitRequest.java
+++ src/main/java/com/example/wanted/dto/recruit/request/RecruitRequest.java
@@ -2,23 +2,28 @@ package com.example.wanted.dto.recruit.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
 public class RecruitRequest {
     private long companyId;
     private String position;
     private int compensation;
     private String content;
+    private String location;
     private String skill;
 
+
     @Builder
-    public RecruitRequest(long companyId, String position, int compensation, String content, String skill) {
+    public RecruitRequest(long companyId, String position, int compensation, String content, String location, String skill) {
         this.companyId = companyId;
         this.position = position;
         this.compensation = compensation;
         this.content = content;
+        this.location = location;
         this.skill = skill;
     }
 }
